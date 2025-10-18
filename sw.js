40a203822107e89d876d76d653e018801abcd83e
RAVI KASAUDHAN <91520095+ravivit@users.noreply.github.com>
2025-10-18 23:39:12 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'devconnect-v1';
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
